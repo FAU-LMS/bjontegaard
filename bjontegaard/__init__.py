07ef2acb06d0bf968aaf813db1848525d8f7f67b
labradon <andy.regensky@gmail.com>
2022-09-05 13:27:29 +0200

    Add version specifier to package's __init__.py

--- bjontegaard/__init__.py
+++ bjontegaard/__init__.py
@@ -28,5 +28,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+__version__ = "1.0.0"
+
 from .functions import bd_rate, bd_psnr
 from .evaluate import compare_methods
