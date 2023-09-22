1678d8b266d3935a1ae7abcfaf1c739f5fa7af58
Andy Regensky <andy.regensky@gmail.com>
2023-09-22 11:34:02 +0200

    Bump version number

--- bjontegaard/__init__.py
+++ bjontegaard/__init__.py
@@ -28,7 +28,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 from .functions import bd_linear, bd_rate, bd_psnr, plot_rcd
 from .evaluate import compare_methods
